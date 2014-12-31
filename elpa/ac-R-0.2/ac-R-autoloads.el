f04791385c5a52c2be8dd278806b5746fcceb6e7
Wakabaya <wakaba@wakabaya.info>
2015-01-01 02:11:26 +0900

    first commit

---
+++ elpa/ac-R-0.2/ac-R-autoloads.el
@@ -0,0 +1,15 @@
+;;; ac-R-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ac-R.el") (21667 50813 738112 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ac-R-autoloads.el ends here
---
+++ init.el
@@ -0,0 +1,96 @@
+;;ctrl-hでバックスペースが使える
+(global-set-key "\C-h" 'delete-backward-char)
+
+;; カーソル位置の行数をモードライン行に表示する
+(line-number-mode 1)
+(global-linum-mode t)
+
+;;日本語環境下
+(set-language-environment "Japanese")
+
+;; 対応する括弧をハイライトする
+(show-paren-mode 1)
+
+;; 対応する括弧の色の設定
+(setq show-paren-style 'mixed)
+(set-face-background 'show-paren-match-face "grey")
+(set-face-foreground 'show-paren-match-face "black")
+
+;; エラー音をならなくする
+(setq ring-bell-function 'ignore)
+
+;; オープニングメッセージを表示しない
+(setq inhibit-startup-message t)
+
+;; バックアップファイルを作成しない
+;; 危険なので、注意して下さい
+(setq make-backup-files nil)
+
+;; カーソル位置の桁数をモードライン行に表示する
+(column-number-mode 1)
+
+;; auto-install設定
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/auto-install/"))
+(require 'auto-install)
+(auto-install-update-emacswiki-package-name t)
+(auto-install-compatibility-setup) 
+
+;; MELPA追加リポジトリの設定
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;;{}を自動入力
+(electric-pair-mode t)
+
+;; ruby coding
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ruby mode
+(autoload 'ruby-mode "ruby-mode"
+  "Mode for editing ruby source files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$latex " . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+
+;;ruby-mode indent
+(setq ruby-deep-indent-paren-style nil)
+
+;; ruby electric
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+(setq ruby-electric-expand-delimiters-list nil)
+
+;; flymake-ruby　静的解析
+(require 'flymake-ruby)
+(add-hook 'ruby-mode-hook 'flymake-ruby-load)
+(add-hook 'ruby-mode-hook 'flycheck-mode)
+
+;; ruby end
+(require 'ruby-end)
+
+;;inf-ruby
+(require 'inf-ruby)
+(setq inf-ruby-default-implementation "pry")
+(setq inf-ruby-eval-binding "Pry.toplevel_binding")
+(add-hook 'inf-ruby-mode-hook 'ansi-color-for-comint-mode-on)
+
+;; smart-compile
+(require 'smart-compile)
+(global-set-key (kbd "C-x c") 'smart-compile)
+(global-set-key (kbd "C-x C-x") (kbd "C-x c C-m"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'cl)
+
+;; magit
+(require 'magit)
+
+;; ediff
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+(setq ediff-split-window-function 'split-window-horizontally)
+
+;; auto-complete
+(require 'auto-complete)
+(require 'auto-complete-config)  
+(global-auto-complete-mode t)
